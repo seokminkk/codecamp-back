6e229a7360a90f193adb47d85bcf8a0a037f445a
seokmin <seokmin0016@gmail.com>
2023-02-23 00:08:48 +0900

    폰라우터추가

--- class/04/04-02-rest-api-with-express-board.ts
+++ class/04/04-02-rest-api-with-express-board.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { checkValidationPhone, getToken, sendTokenToSMS } from "./phone";
 
 const app = express();
 app.use(express.json());
@@ -32,6 +33,25 @@ app.get("/boards", (req, res) => {
 app.post("/boards", (req, res) => {
   console.log(req.body);
   res.send("게시물 등록 성공");
+});
+
+app.post("/tokens/phone", (req, res) => {
+  // req.body 객체의 myphone의 값을 myphone이라는 변수에 담기.
+  const myphone = req.body.myphone;
+
+  // 1. 휴대폰번호 자릿수 맞는지 확인하기
+  const isValid = checkValidationPhone(myphone);
+  if (isValid) {
+    // 2. 핸드폰 토큰 6자리 만들기
+    const mytoken = getToken();
+
+    // 3. 핸드폰번호에 토큰 전송하기
+
+    mytoken && sendTokenToSMS(myphone, mytoken);
+    res.send("인증완료!!!");
+  } else {
+    res.send("에러");
+  }
 });
 
 app.listen(3000, () => {
